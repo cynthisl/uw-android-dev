276ca4501925fd5ba3be82f307018e5552dd904e
Cynthia <cynthslee@gmail.com>
2014-04-16 00:53:29 -0700

    basic functionality done, still needs graphics

---
+++ Homework 251/src/com/cynthisl/Homework_251/Authorized.java
@@ -0,0 +1,35 @@
+package com.cynthisl.Homework_251;
+
+import android.app.ActionBar;
+import android.app.Activity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+
+public class Authorized extends Activity {
+
+    TextView emailText;
+    TextView passwordText;
+
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.authorized);
+        ActionBar ab = getActionBar();
+        ab.hide();
+
+        Bundle b = getIntent().getExtras();
+        String email = null;
+        String pass = null;
+        if (b != null) {
+            email = b.getString("email");
+            pass = b.getString("password");
+        }
+
+        emailText = (TextView) findViewById(R.id.emailText);
+        passwordText = (TextView) findViewById(R.id.passwordText);
+
+        emailText.setText(email);
+        passwordText.setText(pass);
+
+    }
+}
--- Homework 251/src/com/cynthisl/Homework_251/SignIn.java
+++ Homework 251/src/com/cynthisl/Homework_251/SignIn.java
@@ -1,9 +1,13 @@
 package com.cynthisl.Homework_251;
 
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class SignIn extends Activity {
     /**
@@ -16,11 +20,45 @@ public class SignIn extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(R.layout.sign_in);
+        ActionBar ab = getActionBar();
+        ab.hide();
 
         emailInput = (EditText) findViewById(R.id.emailInput);
         passwordInput = (EditText) findViewById(R.id.passwordInput);
         signInButton = (Button) findViewById(R.id.signInButton);
 
+        signInButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(validateInputs()){
+                    launchAuthorizedActivity();
+                }
+            }
+        });
+
+    }
+
+    private boolean validateInputs(){
+        if(emailInput.getText().length() == 0){
+            Toast.makeText(getApplicationContext(), "Please input email", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        if(passwordInput.getText().length() == 0){
+            Toast.makeText(getApplicationContext(), "Please input password", Toast.LENGTH_SHORT).show();
+            return false;
+        }
+
+        return true;
+    }
+    /**
+     * successful sign in
+     */
+    private void launchAuthorizedActivity(){
+        Intent i = new Intent(this, Authorized.class);
+        i.putExtra("email", emailInput.getText().toString());
+        i.putExtra("password", passwordInput.getText().toString());
+        startActivity(i);
     }
 }
